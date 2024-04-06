d8baf91301402a00ee7382b5443860cebfff7c35
AgamMalik <agam.malik31@gmail.com>
2024-04-06 21:36:07 +0530

    working on learn page (not complete yet)

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,20 +1,89 @@
-// import { Sidebar } from "@/components/sidebar";
-// import { MobileHeader } from "@/components/mobile-header";
+import { redirect } from "next/navigation";
 
-type Props = {
-  children: React.ReactNode;
-};
+import { Promo } from "@/components/promo";
+import { Quests } from "@/components/quests";
+import { FeedWrapper } from "@/components/feed-wrapper";
+import { UserProgress } from "@/components/user-progress";
+import { StickyWrapper } from "@/components/sticky-wrapper";
+import { lessons, units as unitsSchema } from "@/db/schema";
+import {
+  getCourseProgress,
+  getLessonPercentage,
+  getUnits,
+  getUserProgress,
+  getUserSubscription,
+} from "@/db/queries";
 
-const MainLayout = ({ children }: Props) => {
+import { Unit } from "./unit";
+import { Header } from "./header";
+
+const LearnPage = async () => {
+  const userProgressData = getUserProgress();
+  const courseProgressData = getCourseProgress();
+  const lessonPercentageData = getLessonPercentage();
+  const unitsData = getUnits();
+  const userSubscriptionData = getUserSubscription();
+
+  const [
+    userProgress,
+    units,
+    courseProgress,
+    lessonPercentage,
+    userSubscription,
+  ] = await Promise.all([
+    userProgressData,
+    unitsData,
+    courseProgressData,
+    lessonPercentageData,
+    userSubscriptionData,
+  ]);
+
+  if (!userProgress || !userProgress.activeCourse) {
+    redirect("/courses");
+  }
+
+  if (!courseProgress) {
+    redirect("/courses");
+  }
+
+  const isPro = !!userSubscription?.isActive;
+
   return (
-    <>
-      {/* <MobileHeader />
-      <Sidebar className="hidden lg:flex" /> */}
-      <main className="lg:pl-[256px] h-full pt-[50px] lg:pt-0">
-        <div className="max-w-[1056px] mx-auto pt-6 h-full">{children}</div>
-      </main>
-    </>
+    <div className="flex flex-row-reverse gap-[48px] px-6">
+      <StickyWrapper>
+        <UserProgress
+          activeCourse={userProgress.activeCourse}
+          hearts={userProgress.hearts}
+          points={userProgress.points}
+          hasActiveSubscription={isPro}
+        />
+        {!isPro && <Promo />}
+        <Quests points={userProgress.points} />
+      </StickyWrapper>
+      <FeedWrapper>
+        <Header title={userProgress.activeCourse.title} />
+        {units.map((unit) => (
+          <div key={unit.id} className="mb-10">
+            <Unit
+              id={unit.id}
+              order={unit.order}
+              description={unit.description}
+              title={unit.title}
+              lessons={unit.lessons}
+              activeLesson={
+                courseProgress.activeLesson as
+                  | (typeof lessons.$inferSelect & {
+                      unit: typeof unitsSchema.$inferSelect;
+                    })
+                  | undefined
+              }
+              activeLessonPercentage={lessonPercentage}
+            />
+          </div>
+        ))}
+      </FeedWrapper>
+    </div>
   );
 };
 
-export default MainLayout;
+export default LearnPage;
